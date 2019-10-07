db66957f089a5903b414f6cb4103a629b7c38758
quoctienphampham09040908 <quoctienphampham09040908@gmail.com>
2019-10-07 23:04:10 +0700

    navbar

---
+++ app/views/layouts/header.php
@@ -0,0 +1,29 @@
+<header>
+	<nav class="nav-bar-main">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li>
+				<a href="#">Product</a>
+				<ul class="dropdown-main">
+					<li>
+						<div class="item-nav">
+						   <div class="thumb">
+						   	<img src="<?= _storage.'vio.jpg' ?>" alt="">
+						   </div>
+						   <div class="heading">
+						   	 <span>Vio 1</span>
+						   </div>
+					    </div>
+				     </li>
+					<li>Vio2</li>
+					<li>Vio3</li>
+				</ul>  
+			</li>
+			<li class="logo">
+				<img src="./logo.png" alt="">
+			</li>
+			<li><a href="">About</a></li>
+			<li><a href="">Contact</a></li>
+		</ul>
+	</nav>
+</header>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   @define('_view', './app/views/');
   @define('_controller', './app/controllers/');
   @define('_lib', './lib/');
+  @define('_storage', './storage/');
   include_once _lib."config.php";
   include_once _lib."functions.php";
   include_once _lib."class.database.php";
@@ -14,11 +15,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./app/assets/fontawesome.min.css">
+    <script src="./app/assets/js/fontawesome.min.js"></script>
+    <link rel="stylesheet" href="./app/assets/js/jquery-3.4.1.min">
+    <link rel="stylesheet" href="./app/assets/js/bootstrap.min.js">
+    <link rel="stylesheet" href="./app/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./app/assets/css/style.css">
 </head>
 <body>
-  <?= $_REQUEST['param-1'] ?>
-  <?= $_REQUEST['p'] ?>1
+  <?php include _view.'layouts/header.php'  ?>
+<!--   <?= $_REQUEST['param-1'] ?>
+  <?= $_REQUEST['p'] ?>1 -->
+  <?php include _view.'layouts/footer.php'  ?>
 </body>
 </html>
